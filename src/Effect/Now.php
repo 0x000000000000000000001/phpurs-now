6cb820948c567da14c483bf21662d00ec22f79b5
0x1 <kevin.francart3@gmail.com>
2026-07-07 17:18:12 +0200

    Add Now.php

--- src/Effect/Now.php
+++ src/Effect/Now.php
@@ -1,14 +1,13 @@
 <?php
 
-$now = function () {
-    return microtime(true) * 1000;
+$now = function() {
+    return round(microtime(true) * 1000);
 };
 
-$getTimezoneOffset = function () {
-    return - (date('Z') / 60);
+$getTimezoneOffset = function() {
+    return -(date("Z") / 60);
 };
 
 $exports['now'] = $now;
 $exports['getTimezoneOffset'] = $getTimezoneOffset;
-
 return $exports;
